2028d18a7722f05da80b4b12d33f3c932c306956
Petr <petr-sed@rambler.ru>
2019-06-11 12:29:26 +0300

    Сделал основное и вторичное меню динамическими

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,14 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
+links_menu = [
+    {'href': 'main', 'name': 'HOME'},
+    {'href': 'products', 'name': 'PRODUCTS'},
+    {'href': 'main', 'name': 'HISTORY'},
+    {'href': 'main', 'name': 'SHOWROOM'},
+    {'href': 'contact', 'name': 'CONTACT'},
+]
+
+links_sec_menu = [
+    {'href': 'products', 'name': 'все'},
+    {'href': 'products', 'name': 'дом'},
+    {'href': 'products', 'name': 'офис'},
+    {'href': 'products', 'name': 'модерн'},
+    {'href': 'products', 'name': 'классика'},
+]
+
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {'links_menu': links_menu, 'title': 'title'}
+    return render(request, 'mainapp/index.html', content)
 
 def products(request):
-    return render(request, 'mainapp/catalog.html')
+    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title'}
+    return render(request, 'mainapp/catalog.html', content)
 
 def contact(request):
-    return render(request, 'mainapp/contacts.html')
+    content = {'links_menu': links_menu, 'title': 'title'}
+    return render(request, 'mainapp/contacts.html', content)
 
 def deal(request):
-    return render(request, 'mainapp/deals.html')
+    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title'}
+    return render(request, 'mainapp/deals.html', content)
