0dbf0227c28a26003a313cef92dbe57f6f07b41f
Petr <petr-sed@rambler.ru>
2019-06-11 22:38:34 +0300

    динамическое отображение title из json

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,19 +9,20 @@ with open("db.json", "r", encoding='utf-8') as read_file:
 links_menu = data['links_menu']
 links_sec_menu = data['links_sec_menu']
 product_2 = data['product_2']
+titles = data['titles']
 
 def main(request):
-    content = {'links_menu': links_menu, 'title': 'title', 'product_2': product_2}
+    content = {'links_menu': links_menu, 'titles': titles, 'product_2': product_2}
     return render(request, 'mainapp/index.html', content)
 
 def products(request):
-    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title', 'product_2': product_2}
+    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'titles': titles, 'product_2': product_2}
     return render(request, 'mainapp/catalog.html', content)
 
 def contact(request):
-    content = {'links_menu': links_menu, 'title': 'title'}
+    content = {'links_menu': links_menu, 'titles': titles}
     return render(request, 'mainapp/contacts.html', content)
 
 def deal(request):
-    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title', 'product_2': product_2}
+    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'titles': titles, 'product_2': product_2}
     return render(request, 'mainapp/deals.html', content)
