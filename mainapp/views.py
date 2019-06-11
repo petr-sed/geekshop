5550881739b6db8537df169296027c63df72b9bd
Petr <petr-sed@rambler.ru>
2019-06-11 17:16:36 +0300

    сделал подшаблон для блока с двумя продуктами для index и catalog

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,13 +8,14 @@ with open("db.json", "r", encoding='utf-8') as read_file:
 
 links_menu = data['links_menu']
 links_sec_menu = data['links_sec_menu']
+product_2 = data['product_2']
 
 def main(request):
-    content = {'links_menu': links_menu, 'title': 'title'}
+    content = {'links_menu': links_menu, 'title': 'title', 'product_2': product_2}
     return render(request, 'mainapp/index.html', content)
 
 def products(request):
-    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title'}
+    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title', 'product_2': product_2}
     return render(request, 'mainapp/catalog.html', content)
 
 def contact(request):
@@ -22,5 +23,5 @@ def contact(request):
     return render(request, 'mainapp/contacts.html', content)
 
 def deal(request):
-    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title'}
+    content = {'links_menu': links_menu, 'links_sec_menu': links_sec_menu, 'title': 'title', 'product_2': product_2}
     return render(request, 'mainapp/deals.html', content)
