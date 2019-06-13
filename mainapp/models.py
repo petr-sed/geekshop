f678d7aceb76be282898b1830ba82ba17efcae6f
Petr <petr-sed@rambler.ru>
2019-06-13 14:52:51 +0300

    русские псевдоимы для имен моделей и их множественные числа

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class ProductCategory (models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
     name = models.CharField(verbose_name='имя', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание', blank=True)
 
@@ -10,6 +14,10 @@ class ProductCategory (models.Model):
         return  self.name
 
 class Product(models.Model):
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128)
     image = models.ImageField(upload_to='products_images', blank=True)
