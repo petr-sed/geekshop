2d28f5f324832d3ae33499b22a4d4b95f368b5a2
Petr <petr-sed@rambler.ru>
2019-06-16 22:21:17 +0300

    20я минута урока

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp',
+    'mainapp.apps.MainappConfig',
+    'authapp.apps.AuthappConfig'
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 
 # Internationalization
