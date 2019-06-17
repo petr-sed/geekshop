64593d077fedd9a645b89d8c5460c325a29ce04b
Petr <petr-sed@rambler.ru>
2019-06-17 11:36:59 +0300

    import UpdateView in authapp.views, include authapp.urls in urls, fix autapp.urls

--- authapp/urls.py
+++ authapp/urls.py
@@ -6,6 +6,6 @@ app_name = 'authapp'
 urlpatterns = [
     path('register/', authapp.register, name='register'),
     path('login/', authapp.login, name='login'),
-    path('edit/<int:pk>/', authapp.EditView(), name='edit'),
-    path('logout/', authapp.logaut, name='logout')
+    path('edit/<int:pk>/', authapp.EditView.as_view(), name='edit'),
+    path('logout/', authapp.logout, name='logout')
 ]
