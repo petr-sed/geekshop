c27c96b29f6d8214c780c95f236818c87e946685
Petr <petr-sed@rambler.ru>
2019-06-17 11:20:01 +0300

    + authapp.templates.authapp, + authapp.views, + authapp.urls

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import ShopUser
 
 # Create your views here.
+
+def register(request):
+    return render(request, 'authapp/register.html')
+
+def login(reauest):
+    return render(reauest, 'authapp/login.html')
+
+def edit(request):
+    return render(request, 'authapp/edit.html')
+
+def logout(request):
+    return render(request, 'authapp/logout.html')
