47bc1be236b2c83d9b455177b1c4c872d05b7c5c
Petr <petr-sed@rambler.ru>
2019-06-19 21:46:43 +0300

    mainapp.views

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,7 +7,6 @@ from .models import Product, ProductCategory
 with open("db.json", "r", encoding='utf-8') as read_file:
     data = json.load(read_file)
 
-links_menu = data['links_menu']
 links_sec_menu = ProductCategory.objects.all()
 product_2 = data['product_2']
 titles = data['titles']
@@ -15,7 +14,6 @@ titles = data['titles']
 def main(request):
     products = Product.objects.all()
     content = {
-                'links_menu': links_menu,
                 'titles': titles,
                 'products_4': products[:4],
                 'products_6': products[4:10],
@@ -32,7 +30,6 @@ def products(request, pk=None):
         products = Product.objects.all()[:12]
         media = '../media/'
     content = {
-               'links_menu': links_menu,
                'links_sec_menu': links_sec_menu,
                'products_12': products,
                'titles': titles,
@@ -43,14 +40,12 @@ def products(request, pk=None):
 
 def contact(request):
     content = {
-                'links_menu': links_menu,
                 'titles': titles
               }
     return render(request, 'mainapp/contacts.html', content)
 
 def deal(request):
     content = {
-                'links_menu': links_menu,
                 'links_sec_menu': links_sec_menu,
                 'titles': titles,
                 'product_2': product_2
