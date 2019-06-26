b82737257d7e2cc282b20ebe40e0a425d8643f27
Petr <petr-sed@rambler.ru>
2019-06-26 22:31:58 +0300

    category and product: update, read, create

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
     path('users/update/<int:pk>/', adminapp.user_update, name='user_update'),
     path('users/delete/<int:pk>/', adminapp.user_delete, name='user_delete'),
 
-    path('categories/create/', adminapp.category_create, name='category_create'),
+    path('categories/create/', adminapp.CategoryCreateView.as_view(), name='category_create'),
     path('categories/read/', adminapp.CategoryListView.as_view(), name='categories'),
-    path('categories/update/<int:pk>/', adminapp.category_update, name='category_update'),
+    path('categories/update/<int:pk>/', adminapp.CategoryUpdateView.as_view(), name='category_update'),
     path('categories/delete/<int:pk>/', adminapp.category_delete, name='category_delete'),
 
-    path('products/create/category/<int:pk>/', adminapp.product_create, name='product_create'),
+    path('products/create/category/<int:pk>/', adminapp.ProductsCreateView.as_view(), name='product_create'),
     path('products/read/', adminapp.ProductListView.as_view(), name='products'),
     path('products/read/category/<int:category_pk>/', adminapp.ProductListView.as_view(), name='products_by_category'),
     path('products/read/<int:pk>/', adminapp.ProductDetailView.as_view(), name='product_read'),
-    path('products/update/<int:pk>/', adminapp.product_update, name='product_update'),
+    path('products/update/<int:pk>/', adminapp.ProductsUpdateView.as_view(), name='product_update'),
     path('products/delete/<int:pk>/', adminapp.product_delete, name='product_delete'),
 ]
 
