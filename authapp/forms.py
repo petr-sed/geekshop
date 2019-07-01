462670528aaa0b20a36e2b0cdc4a14c65d7d18f2
Petr <petr-sed@rambler.ru>
2019-07-01 12:09:23 +0300

    +UserListView

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
-
 from .models import ShopUser
 
 
