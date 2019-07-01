fd97189a219550431f809ec8bcd83080337508f2
Petr <petr-sed@rambler.ru>
2019-07-01 21:53:59 +0300

    +paginator

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,6 +4,7 @@ import mainapp.views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', mainapp.products, name='index'),
-    path('<int:pk>/', mainapp.products, name='category'),
+    path('category/0/', mainapp.products, name='index'),
+    path('category/<int:pk>/', mainapp.products, name='category'),
+    path('category/<int:pk>/page/<int:page>/', mainapp.products, name='page'),
 ]
