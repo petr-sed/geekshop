8d27f352d91d68d497067cee7f89a288d9de3573
Petr <petr-sed@rambler.ru>
2019-07-03 11:11:24 +0300

    Админка: добавление полей, фильтров, поиска, функциональных  полей, блоков TabularInline, ограничение queryset, создание двух и более классов на одну модель

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,5 +3,15 @@ from .models import ProductCategory, Product
 
 # Register your models here.
 
-admin.site.register(ProductCategory)
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'name', 'category', 'is_hot', 'is_active',
+    list_filter = 'is_hot', 'is_active',
+    search_fields = 'name', 'category__name', 'short_desc', 'description',
+    readonly_fields = 'quantity',
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = 'name', 'is_active',
+    list_filter = 'is_active',
+    search_fields = 'name', 'description',
