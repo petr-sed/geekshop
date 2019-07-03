7b0da053d26fe0a620258173d88626c6b7068644
Petr <petr-sed@rambler.ru>
2019-07-03 13:20:35 +0300

    создан шаблонный фильтр media_folder_products и применен в шаблонах index, catalog, inc_prod_2

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,7 +4,7 @@ import mainapp.views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('category/0/', mainapp.products, name='index'),
+    path('category/0/page/1', mainapp.products, name='index'),
     path('category/<int:pk>/', mainapp.products, name='category'),
     path('category/<int:pk>/page/<int:page>/', mainapp.products, name='page'),
 ]
